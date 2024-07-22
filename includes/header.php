debbe2b8a8b76e3305e968240cbb8381ed0bf7d6
Abdul rehman Ali <118043624+Abdul-Rehman124@users.noreply.github.com>
2024-07-22 13:09:25 +0500

    minor changes

--- includes/header.php
+++ includes/header.php
@@ -14,105 +14,181 @@
             </div>
             <div class="col-md-8 d-flex justify-content-end">
                 <div class="header-item">
-                   <p class="mb-0">Free trial endsin 7 days</p>
-                   <a href="" class="upgrade-button">Upgrade</a>
+                    <p class="mb-0">Free trial endsin 7 days</p>
+                    <a href="" class="upgrade-button">Upgrade</a>
                 </div>
                 <div class="header-item">
-                   <a href="" class="chat-link"><i class="fa-solid fa-headset"></i> Talk to an expert</a>
+                    <a href="" class="chat-link"><i class="fa-solid fa-headset"></i> Talk to an expert</a>
                 </div>
                 <div class="header-item">
-                   <div class="dropdown">
-                       <button class="btn  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                         Help
-                       </button>
-                       <ul class="dropdown-menu">
-                         <li><a class="dropdown-item" href="#"><i class="fa-solid fa-play"></i> Watch video tutuorials </a></li>
-                         <li><a class="dropdown-item" href="#"><i class="fa-solid fa-question"></i> Visit the help center</a></li>
-                         
-                       </ul>
-                     </div>
+                    <div class="dropdown">
+                        <button class="btn  dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Help
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#"><i class="fa-solid fa-play"></i> Watch video
+                                    tutuorials </a></li>
+                            <li><a class="dropdown-item" href="#"><i class="fa-solid fa-question"></i> Visit the help
+                                    center</a></li>
+
+                        </ul>
+                    </div>
                 </div>
                 <div class="header-item three-icons d-flex align-items-center">
-                   <a href="javascript:;" type="button" class="btn " data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Tooltip on bottom">
-                       <i class="fa-solid fa-person"></i>
-                   </a>
-                   <a href="javascript:;" type="button" class="btn " data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Tooltip on bottom">
-                       <i class="fa-regular fa-comments"></i>
-                   </a>
-                   <a href="javascript:;" type="button" class="btn " data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Tooltip on bottom">
-                       <i class="fa-regular fa-bell"></i>
-                   </a>
+                    <a href="javascript:;" type="button" class="btn " data-bs-toggle="tooltip"
+                        data-bs-placement="bottom" data-bs-title="Tooltip on bottom">
+                        <i class="fa-solid fa-person"></i>
+                    </a>
+                    <a href="javascript:;" type="button" class="btn " data-bs-toggle="tooltip"
+                        data-bs-placement="bottom" data-bs-title="Tooltip on bottom">
+                        <i class="fa-regular fa-comments"></i>
+                    </a>
+                    <a href="javascript:;" type="button" class="btn " data-bs-toggle="tooltip"
+                        data-bs-placement="bottom" data-bs-title="Tooltip on bottom">
+                        <i class="fa-regular fa-bell"></i>
+                    </a>
                 </div>
                 <div class="header-item profile-section">
-                   <div class="profile">
-                       <img src="assets/images/profile.jpg" alt="">
-                       
-                   </div>
-                   <div class="dropdown">
-                       <button class="btn  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span>Andy cooper</span>
-                       </button>
-                       <ul class="dropdown-menu">
-                         <li><a class="dropdown-item" href="#"><i class="fa-solid fa-person"></i> Profile</a></li>
-                         <li><a class="dropdown-item" href="#"><i class="fa-solid fa-cog"></i> Settings</a></li>
-                         <li><a class="dropdown-item" href="#"><i class="fa-solid fa-sign-out"></i> Logout</a></li>
-                       </ul>
-                     </div>
+                    <div class="profile">
+                        <img src="assets/images/profile.jpg" alt="">
+
+                    </div>
+                    <div class="dropdown">
+                        <button class="btn  dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <span>Andy cooper</span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#"><i class="fa-solid fa-person"></i> Profile</a></li>
+                            <li><a class="dropdown-item" href="#"><i class="fa-solid fa-cog"></i> Settings</a></li>
+                            <li><a class="dropdown-item" href="#"><i class="fa-solid fa-sign-out"></i> Logout</a></li>
+                        </ul>
+                    </div>
                 </div>
-               </div>
-            
+            </div>
+
         </div>
     </div>
 </header>
 <div class="sidebar">
     <div class="main-section">
-   <div class="item first-item">
-    <ul>
-        <li><a href="javascript:;"><div class="icon icon-1 chevron"><i class="chevron-icon fa-solid fa-chevron-right "></i></div></a> </li>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-table-cells-large"></i></div> <span class="d-none">Overview</span><div class="three-dots">...</div></a> </li>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-bars"></i></div> <span class="d-none">Activity</span><div class="three-dots">...</div></a> </li>
-    </ul>
-   </div>
-   <div class="item second-item">
-    <ul>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-user"></i></div> <span class="d-none">Users</span><div class="three-dots">...</div></a> </li>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-user-group"></i></div> <span class="d-none">Smart Groups</span><div class="three-dots">...</div></a> </li>
-    </ul>
-   </div>
-   <div class="item third-item">
-    <h6 class="d-none">Communications</h6>
-    <ul>
-        <li><a href=""><div class="icon"><i class="fa-regular fa-comments"></i></div> <span class="d-none">Chat</span><div class="three-dots">...</div></a> </li>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-envelope"></i></div> <span class="d-none">Updates</span><div class="three-dots">...</div></a> </li>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-address-book"></i></div> <span class="d-none">Directory</span><div class="three-dots">...</div></a> </li>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-calendar-days"></i></div> <span class="d-none">Events</span><div class="three-dots">...</div></a> </li>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-book"></i></div> <span class="d-none">Knowladge Base</span><div class="three-dots">...</div></a> </li>
-    </ul>
-   </div>
-   <div class="item fourth-item">
-    <h6 class="d-none">Operations</h6>
-    <ul>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-stopwatch"></i></div> <span class="d-none">Time Clock</span><div class="three-dots">...</div></a> </li>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-file-waveform"></i></div> <span class="d-none">Forms</span><div class="three-dots">...</div></a> </li>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-calendar-days"></i></i></div> <span class="d-none">Jobs Scheduling</span><div class="three-dots">...</div></a> </li>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-clipboard-check"></i></div> <span class="d-none">Quick Tasks</span><div class="three-dots">...</div></a> </li>
-    </ul>
-   </div>
-   <div class="item fifth-item">
-    <h6 class="d-none">HR & Skills</h6>
-    <ul>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-stopwatch"></i></div> <span class="d-none">Time Clock</span><div class="three-dots">...</div></a> </li>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-file-waveform"></i></div> <span class="d-none">Forms</span><div class="three-dots">...</div></a> </li>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-calendar-days"></i></i></div> <span class="d-none">Jobs Scheduling</span><div class="three-dots">...</div></a> </li>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-clipboard-check"></i></div> <span class="d-none">Quick Tasks</span><div class="three-dots">...</div></a> </li>
-    </ul>
-   </div>
-</div>
-<div class="add-section item d-none">
+        <div class="item first-item">
+            <ul>
+                <li><a href="javascript:;">
+                        <div class="icon icon-1 chevron"><i class="chevron-icon fa-solid fa-chevron-right "></i></div>
+                    </a> </li>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-table-cells-large"></i></div> <span
+                            class="d-none">Overview</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-bars"></i></div> <span class="d-none">Activity</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+            </ul>
+        </div>
+        <div class="item second-item">
+            <ul>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-user"></i></div> <span class="d-none">Users</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-user-group"></i></div> <span class="d-none">Smart
+                            Groups</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+            </ul>
+        </div>
+        <div class="item third-item">
+            <h6 class="d-none">Communications</h6>
+            <ul>
+                <li><a href="">
+                        <div class="icon"><i class="fa-regular fa-comments"></i></div> <span class="d-none">Chat</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-envelope"></i></div> <span class="d-none">Updates</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-address-book"></i></div> <span
+                            class="d-none">Directory</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-calendar-days"></i></div> <span
+                            class="d-none">Events</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-book"></i></div> <span class="d-none">Knowladge
+                            Base</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+            </ul>
+        </div>
+        <div class="item fourth-item">
+            <h6 class="d-none">Operations</h6>
+            <ul>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-stopwatch"></i></div> <span class="d-none">Time
+                            Clock</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-file-waveform"></i></div> <span
+                            class="d-none">Forms</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-calendar-days"></i></i></div> <span class="d-none">Jobs
+                            Scheduling</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-clipboard-check"></i></div> <span class="d-none">Quick
+                            Tasks</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+            </ul>
+        </div>
+        <div class="item fifth-item">
+            <h6 class="d-none">HR & Skills</h6>
+            <ul>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-stopwatch"></i></div> <span class="d-none">Time
+                            Clock</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-file-waveform"></i></div> <span
+                            class="d-none">Forms</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-calendar-days"></i></i></div> <span class="d-none">Jobs
+                            Scheduling</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+                <li><a href="">
+                        <div class="icon"><i class="fa-solid fa-clipboard-check"></i></div> <span class="d-none">Quick
+                            Tasks</span>
+                        <div class="three-dots">...</div>
+                    </a> </li>
+            </ul>
+        </div>
+    </div>
+    <div class="add-section item d-none">
 
-    <ul>
-        <li><a href=""><div class="icon"><i class="fa-solid fa-plus"></i></div> <span class="d-none">Add Section</span><div class="three-dots">...</div></a> </li>
-    </ul>
+        <ul>
+            <li><a href="">
+                    <div class="icon"><i class="fa-solid fa-plus"></i></div> <span class="d-none">Add Section</span>
+                    <div class="three-dots">...</div>
+                </a> </li>
+        </ul>
 
-</div>
+    </div>
 </div>
