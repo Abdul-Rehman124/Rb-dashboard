69143356b57fe00a81860245920c625893f06176
Ausaf memon <ausafdevelopor@gmail.com>
2024-07-26 15:47:35 -0700

    changes

---
+++ Chats.php
@@ -0,0 +1,8 @@
+<?php include 'includes/head.php'; ?>
+<?php include 'includes/header.php'; ?>
+<!-- First-section -->
+
+
+
+<?php include 'includes/footer.php'; ?>
+<?php include 'includes/scripts.php'; ?>
--- Smart-Group.php
+++ Smart-Group.php
@@ -33,7 +33,55 @@
        </div>
     </div>
  </section>
-
+<!-- Second-section -->
+ <section class="about-sec">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-12">
+               <div class="content-sec">
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item mb-4">
+                      <h2 class="accordion-header">
+                        <button class="accordion-button border-yellow" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Segment name <img src="assets/images/cicle-plus.webp" alt="">
+                        </button>
+                      </h2>
+                      <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                          <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                      </div>
+                    </div>
+                    <div class="accordion-item mb-4">
+                      <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Segment name
+                        </button>
+                      </h2>
+                      <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                          <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                      </div>
+                    </div>
+                    <div class="accordion-item">
+                      <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Segment name
+                        </button>
+                      </h2>
+                      <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                          <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+               </div>
+            </div>
+        </div>
+    </div>
+ </section>
 
 
 <?php include 'includes/footer.php'; ?>
